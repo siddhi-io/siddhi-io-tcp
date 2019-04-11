875d8c8d7d3f8d6c4abda271f774dfa41d73ed5c
mohan <mohan.vive@gmail.com>
2019-04-11 17:13:29 +0530

    Revert service deployment info changes

--- component/src/main/java/org/wso2/extension/siddhi/io/tcp/sink/TCPSink.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/tcp/sink/TCPSink.java
@@ -130,7 +130,6 @@ public class TCPSink extends Sink {
     private Option syncOption;
     private Boolean sync = null;
     private String hostAndPort;
-    private ServiceDeploymentInfo serviceDeploymentInfo;
 
     @Override
     protected StateFactory init(StreamDefinition outputStreamDefinition, OptionHolder optionHolder,
@@ -166,7 +165,6 @@ public class TCPSink extends Sink {
         int workerThreads = Integer.parseInt(optionHolder.validateAndGetStaticValue(WORKER_THREADS,
                 DEFAULT_WORKER_THREADS));
         tcpNettyClient = new TCPNettyClient(workerThreads, keepAlive, tcpNoDelay);
-        serviceDeploymentInfo = new ServiceDeploymentInfo(port, false);
         return null;
     }
 
@@ -177,7 +175,7 @@ public class TCPSink extends Sink {
 
     @Override
     protected ServiceDeploymentInfo exposeServiceDeploymentInfo() {
-        return serviceDeploymentInfo;
+        return null;
     }
 
     @Override
